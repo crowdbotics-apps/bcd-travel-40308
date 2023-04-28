05a1fe13b899507c0cfab6d86ba59ff0225f8b2c
Crowdbotics <team@crowdbotics.com>
2023-04-28 13:28:57 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/bcd_travel_40308/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bcd_travel_40308;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "bcd_travel_40308";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bcd-travel-40308",
+  url: "https://bcd-travel-40308.botics.co",
+  api: "https://bcd-travel-40308.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
